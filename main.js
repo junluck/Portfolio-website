c9063cab7311a980967836720a83aa39a6dff76b
junluck <junaindavidsvickerman@gmail.com>
2024-07-21 13:07:52 +0200

    website update

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const humburger = document.querySelector(".humburger-Menu ")
 const hiddenMenu = document.querySelector(".hiddenMenu")
 const button = document.querySelector(".portfolioAnchor");
+location.hash = "";
 
 humburger.addEventListener("click", () =>{
     humburger.classList.toggle("active");
     hiddenMenu.classList.toggle("active");
 })
 
-location.hash = "";
+
+
+
