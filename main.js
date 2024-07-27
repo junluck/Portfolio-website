e18198a402428931f5603522cb18fd5de0b64695
junluck <junaindavidsvickerman@gmail.com>
2024-07-27 10:42:52 +0200

    clean up javascript main.js

--- main.js
+++ main.js
@@ -1,80 +1,88 @@
-//storing classes in variables
-const humburger = document.querySelector(".humburger-Menu ")
-const hiddenMenu = document.querySelector(".hiddenMenu")
-const button = document.querySelector(".portfolioAnchor");
-const portfolio = document.querySelector(".portfolioHeading");
-const editdooTitle = document.querySelector(".editdooTitle");
-const editdooImage = document.querySelector(".buttonAndImage");
-const editdooDesc = document.querySelector(".editdooDescription");
-const editdooFirstPic = document.querySelector(".editdooImage");
-const editdooSecondPic = document.querySelector(".editdooWorkSec");
-const editdooThirdPic = document.querySelector(".editdooFaqSec");
-const rightArrow = document.querySelector(".portfolioButtonRight");
-const leftArrow = document.querySelector(".portfolioButtonLeft");
-const arrayOfPictures = [editdooFirstPic, editdooSecondPic,editdooThirdPic];
 
-
-//page reaload back to home link
+// Reset the hash to an empty string on page reload
 location.hash = "";
 
+// Storing DOM elements in variables
+const humburger = document.querySelector(".humburger-Menu ");
+const hiddenMenu = document.querySelector(".hiddenMenu");
 
-
-//humburger menu that toggles on active class when clicked
+// Toggle the "active" class on the hamburger menu and hidden menu when the hamburger menu is clicked
 humburger.addEventListener("click", () =>{
     humburger.classList.toggle("active");
     hiddenMenu.classList.toggle("active");
 })
 
-//making a slider for portfolio
-function eventListenerForSlider(right,left,array){
+// Function to add event listeners for a slider functionality
+function eventListenerForSlider(right,left,array,arrayOfCircles){
   let count = -1;
+  // Event listener for the right arrow click
   right.addEventListener("click", () =>{
       count += 1;
       if (count === array.length) {
         count = 0;
       }
+      // Adjusting the display property of array items based on the count
       if (count === 0){
         array[count].style.display = "none";
         array[count + 1].style.display = "flex";
-        array[count + 2].style.display = "none";  
+        array[count + 2].style.display = "none"; 
+        arrayOfCircles[count].style.backgroundColor  = "transparent";
+        arrayOfCircles[count + 1].style.backgroundColor = "#6A994E";
+        arrayOfCircles[count + 2].style.backgroundColor  = "transparent";  
       } 
     
       else if (count === 1){
         array[count - 1].style.display = "none";
         array[count].style.display = "none";
-        array[count + 1].style.display = "flex";  
+        array[count + 1].style.display = "flex"; 
+        arrayOfCircles[count - 1].style.backgroundColor  = "transparent";
+        arrayOfCircles[count].style.backgroundColor = "transparent";
+        arrayOfCircles[count + 1].style.backgroundColor  = "#6A994E";
       } 
     
       else if (count === 2) {
         array[count - 2].style.display = "flex";
         array[count - 1].style.display = "none";
         array[count].style.display = "none";  
+        arrayOfCircles[count - 2].style.backgroundColor  = "#6A994E";
+        arrayOfCircles[count - 1].style.backgroundColor = "transparent";
+        arrayOfCircles[count].style.backgroundColor  = "transparent";
       } 
      
     })
       
-
+  // Event listener for the left arrow click
   left.addEventListener("click", ()=>{
         count -= 1;
         if (count < 0 ) {
         count = array.length - 1 ;
       }
+      // Adjusting the display property of array items based on the count
       if (count === 0){
         array[count].style.display = "none";
         array[count + 1].style.display = "flex";
         array[count + 2].style.display = "none";  
+        arrayOfCircles[count].style.backgroundColor  = "transparent";
+        arrayOfCircles[count + 1].style.backgroundColor = "#6A994E";
+        arrayOfCircles[count + 2].style.backgroundColor  = "transparent";
       } 
     
       else if (count === 1){
         array[count - 1].style.display = "none";
         array[count].style.display = "none";
         array[count + 1].style.display = "flex";  
+        arrayOfCircles[count - 1].style.backgroundColor  = "transparent";
+        arrayOfCircles[count].style.backgroundColor = "transparent";
+        arrayOfCircles[count + 1].style.backgroundColor  = "#6A994E";
       } 
     
       else if (count === 2) {
         array[count - 2].style.display = "flex";
         array[count - 1].style.display = "none";
         array[count].style.display = "none";  
+        arrayOfCircles[count - 2].style.backgroundColor  = "#6A994E";
+        arrayOfCircles[count - 1].style.backgroundColor = "transparent";
+        arrayOfCircles[count].style.backgroundColor  = "transparent";
       } 
      
       
@@ -85,10 +93,17 @@ function eventListenerForSlider(right,left,array){
   
 }
 
-eventListenerForSlider(rightArrow,leftArrow,arrayOfPictures);
+// Storing DOM elements in variables and an array
+const rightArrow = document.querySelector(".portfolioButtonRight");
+const leftArrow = document.querySelector(".portfolioButtonLeft");
+const arrayOfPictures = [document.querySelector(".editdooImage"), document.querySelector(".editdooWorkSec"),document.querySelector(".editdooFaqSec")];
+const arrayOfCircles = [document.querySelector(".firstCircle"), document.querySelector(".secondCircle"), document.querySelector(".thirdCircle")];
+
+// Initialize the slider functionality with the right and left arrows and the array of pictures
+eventListenerForSlider(rightArrow, leftArrow, arrayOfPictures, arrayOfCircles );
 
 
-//function that takes projects and appends each project to the projects div
+// Function to append a project heading to the projects div
 const appendProjects = function (headingProject)
 {
   const projects = document.getElementById("projects");
@@ -174,11 +189,14 @@ const observer = new IntersectionObserver(entries =>{
       threshold:0.001,
     })
 
+// Storing DOM elements in variables
+const portfolio = document.querySelector(".portfolioHeading");
+const editdooTitle = document.querySelector(".editdooTitle");
+const editdooImage = document.querySelector(".buttonAndImage");
 //Invoke observe method on elements
 observer.observe(portfolio);
 observer.observe(editdooTitle);
 animateTwoElementsIn.observe(editdooImage);
-
 
 
 
---
+++ projects.js
@@ -0,0 +1,6 @@
+const project = [
+    {
+     projectName: "Porfolio website",
+     
+}
+]
